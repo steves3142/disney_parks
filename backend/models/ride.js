31d82ef52bc5cdc1a7457801e973241fa4ad626b
kavdesilva <kavdesilva@gmail.com>
2022-07-21 20:48:07 -0400

    database complete except for ride images

--- backend/models/ride.js
+++ backend/models/ride.js
@@ -8,7 +8,7 @@ const Ride = new Schema(
         rank: { type: Number, required: true }, // ranked 1-3 out of top 3 per park
         heightRequirement: { type: String, required: true },
         waterRide: { type: Boolean, required: true }, // boolean
-        wheelchairAccessible: { type: Boolean, required: true }, // boolean
+        wheelchairOnRide: { type: Boolean, required: true }, // boolean
         wheelchairTransfer: { type: Boolean, required: true }, // boolean, usually false if wheelchairAccessible = true
         image: { type: String, required: true }
     }
