4015037cd002496db28d74d52d84b7e70a6ce99b
kavdesilva <kavdesilva@gmail.com>
2022-07-22 10:21:28 -0400

    fixed incorrect db tag from users > parks, tasks > rides

--- backend/models/index.js
+++ backend/models/index.js
@@ -2,8 +2,8 @@ const { model } = require('mongoose')
 const ParkSchema = require('./park')
 const RideSchema = require('./ride')
 
-const Park = model('User', ParkSchema)
-const Ride = model('Task', RideSchema)
+const Park = model('Park', ParkSchema)
+const Ride = model('Ride', RideSchema)
 
 module.exports = {
   Park,
